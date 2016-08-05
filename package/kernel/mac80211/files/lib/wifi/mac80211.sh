5636b265adabc107da0da5ce721f4ef30b7c69f8
JamesBond <jamesbond.qiu@foxmail.com>
2016-08-05 13:57:17 +0800

    Open wifi by default and set SSID to Oolite-XXXXXX.

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -115,13 +115,13 @@ config wifi-device  radio$devidx
 $dev_id
 $ht_capab
 	# REMOVE THIS LINE TO ENABLE WIFI:
-	option disabled 1
+	option disabled 0
 
 config wifi-iface
 	option device   radio$devidx
 	option network  lan
 	option mode     ap
-	option ssid     OpenWrt
+	option ssid     Oolite-$(cat /sys/class/net/wlan$devidx/address|awk -F ":" '{print $4""$5""$6}'| tr a-z A-Z)
 	option encryption none
 
 EOF
