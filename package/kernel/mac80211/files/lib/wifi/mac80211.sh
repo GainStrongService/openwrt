772f2553c4d5eb6fd401ce387ff08eb1d6f73248
Joe <joe@ioes.cn>
2021-05-13 15:47:59 +0800

    remove driver name from ssid; uppercase ssid

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -107,9 +107,10 @@ detect_mac80211() {
 		fi
 
 		mac_last_4="$(awk -F ":" '{printf toupper($5$6)}' /sys/class/ieee80211/${dev}/macaddress)"
-		driver="$(grep ^DRIVER= /sys/class/ieee80211/${dev}/device/uevent | awk -F '=' '{print $2}')"
+		#driver="$(grep ^DRIVER= /sys/class/ieee80211/${dev}/device/uevent | awk -F '=' '{print $2}')"
 		board_name="$(awk -F ',' '{print $2}' /tmp/sysinfo/board_name)"
-		ssid="${board_name}_${driver}_${band_name}_${mac_last_4}"
+		ssid="${board_name}_${band_name}_${mac_last_4}"
+		ssid=$(echo $ssid | tr 'a-z' 'A-Z')
 
 		uci -q batch <<-EOF
 			set wireless.radio${devidx}=wifi-device
