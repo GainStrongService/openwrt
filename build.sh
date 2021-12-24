2ef7656395970e5cadf0768038e11cd1cc9cd450
Joe Guo <joe@ioes.cn>
2021-12-24 14:21:23 +0800

    添加编译脚本

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -a Luci-Makefile feeds/luci/collections/luci/Makefile
+cp Oolite-34S-LoRa.config .config
+VERSION=Gainstrong-$(./scripts/getver.sh)
+echo "CONFIG_KERNEL_BUILD_DOMAIN=\"$VERSION\"" >> .config
+make defconfig
+make package/busybox/clean
+make -j9 V=sc
