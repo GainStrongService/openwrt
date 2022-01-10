cfa1d23d3861bc4865511b999dba0d093f589047
Joe Guo <joe@ioes.cn>
2022-01-10 18:20:40 +0800

    取消重编译busybox

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ cp Oolite-34S-LoRa.config .config
 VERSION=Gainstrong-$(./scripts/getver.sh)
 echo "CONFIG_KERNEL_BUILD_DOMAIN=\"$VERSION\"" >> .config
 make defconfig
-make package/busybox/clean
 make -j9 V=sc
