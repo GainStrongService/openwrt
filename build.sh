23fbcfa34db826ac7d4c470be18c66783db46a07
Joe Guo <joe@gainstrong.cn>
2022-05-06 15:09:50 +0800

    Add build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp Oolite-MT7620.config .config
+VERSION=GainStrong-$(./scripts/getver.sh)
+echo "CONFIG_KERNEL_BUILD_DOMAIN=\"$VERSION\"" >> .config
+make package/symlinks
+make defconfig
+make package/busybox/clean
+make -j8 V=sc
