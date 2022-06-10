2c7fdc9218f014d204e769c9343c324bdbde943c
Qiao <guoxiaoqiao@gmail.com>
2022-06-10 10:33:25 +0800

    添加Node.js支持

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@ cp Oolite5-MiniBox3.config .config
 VERSION=Gainstrong-$(./scripts/getver.sh)
 echo "CONFIG_KERNEL_BUILD_DOMAIN=\"$VERSION\"" >> .config
 make package/symlinks
+
+rm ./package/feeds/packages/node &> /dev/null
+rm ./package/feeds/packages/node-* &> /dev/null
+./scripts/feeds install -a -p node
 make defconfig
+
 make package/busybox/clean
-make -j9 V=sc
+make -j8 V=sc
