0bb7746cbb2b21d5de337e54507c75479bddd69a
Joe Guo <joe@gainstrong.cn>
2022-09-14 16:45:18 +0800

    build.sh添加node说明

--- build.sh
+++ build.sh
@@ -7,9 +7,11 @@ VERSION=Gainstrong-$(./scripts/getver.sh)
 echo "CONFIG_KERNEL_BUILD_DOMAIN=\"$VERSION\"" >> .config
 make package/symlinks
 
+# 删除OpenWrt自带的Node，解决不兼容QCA9531的问题
 rm ./package/feeds/packages/node &> /dev/null
 rm ./package/feeds/packages/node-* &> /dev/null
 ./scripts/feeds install -a -p node
+
 make defconfig
 
 make package/busybox/clean
