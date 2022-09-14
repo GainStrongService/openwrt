418b78ca1025bdcd3747ae04ed41b1d47fe43491
Joe Guo <joe@gainstrong.cn>
2022-09-14 16:06:34 +0800

    编译结果加上版本号；配置默认IP

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 source scripts/save-build-env.sh
 
-cp Oolite5-MiniBox3.config .config
+cp PrinterServer.config .config
 VERSION=Gainstrong-$(./scripts/getver.sh)
 echo "CONFIG_KERNEL_BUILD_DOMAIN=\"$VERSION\"" >> .config
 make package/symlinks
