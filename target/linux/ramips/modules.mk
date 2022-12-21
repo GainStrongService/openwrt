9f4334243098ab22ffb65a88fbcf383218e4e716
Joe Guo <joe@gainstrong.cn>
2022-12-21 22:33:30 +0800

    change WM8960 to MAX98357A

--- target/linux/ramips/modules.mk
+++ target/linux/ramips/modules.mk
@@ -118,13 +118,13 @@ define KernelPackage/sound-mt7620
 	CONFIG_SND_RALINK_SOC_I2S \
 	CONFIG_SND_SIMPLE_CARD \
 	CONFIG_SND_SIMPLE_CARD_UTILS \
-	CONFIG_SND_SOC_WM8960
+	CONFIG_SND_SOC_MAX98357A
   FILES:= \
 	$(LINUX_DIR)/sound/soc/ralink/snd-soc-ralink-i2s.ko \
 	$(LINUX_DIR)/sound/soc/generic/snd-soc-simple-card.ko \
 	$(LINUX_DIR)/sound/soc/generic/snd-soc-simple-card-utils.ko \
-	$(LINUX_DIR)/sound/soc/codecs/snd-soc-wm8960.ko
-  AUTOLOAD:=$(call AutoLoad,90,snd-soc-wm8960 snd-soc-ralink-i2s snd-soc-simple-card)
+	$(LINUX_DIR)/sound/soc/codecs/snd-soc-max98357a.ko
+  AUTOLOAD:=$(call AutoLoad,90,snd-soc-max98357a snd-soc-ralink-i2s snd-soc-simple-card)
   $(call AddDepends/sound)
 endef
 
