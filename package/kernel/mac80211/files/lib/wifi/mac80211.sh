a89285e8270b5c8e386596da5872fcd51ef74fde
Joe Guo <joe@gainstrong.cn>
2023-08-11 14:09:45 +0800

    Optimize SSID

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -196,6 +196,11 @@ detect_mac80211() {
 				;;
 		esac
 
+		mac_last_4="$(awk -F ":" '{printf toupper($5$6)}' /sys/class/net/eth0/address)"
+		board_name=$(awk -F ',' '{print $2}' /tmp/sysinfo/board_name | awk -F "-" '{print $1"-"$2}')
+		ssid="${board_name}_${mac_last_4}"
+		ssid=$(echo $ssid | tr 'a-z' 'A-Z')
+
 		uci -q batch <<-EOF
 			set wireless.${name}=wifi-device
 			set wireless.${name}.type=mac80211
@@ -203,13 +208,12 @@ detect_mac80211() {
 			set wireless.${name}.channel=${channel}
 			set wireless.${name}.band=${mode_band}
 			set wireless.${name}.htmode=$htmode
-			set wireless.${name}.disabled=1
 
 			set wireless.default_${name}=wifi-iface
 			set wireless.default_${name}.device=${name}
 			set wireless.default_${name}.network=lan
 			set wireless.default_${name}.mode=ap
-			set wireless.default_${name}.ssid=OpenWrt
+			set wireless.default_${name}.ssid=${ssid}
 			set wireless.default_${name}.encryption=none
 EOF
 		uci -q commit wireless
